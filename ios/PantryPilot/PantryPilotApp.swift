1eabedfebb1f959bb66f6a22f459302e63047f72
Jintao Jiang <jintaojiang@jintaodembp.mynetworksettings.com>
2026-06-12 10:07:14 -0400

    Use explicit SwiftData store for app data

--- ios/PantryPilot/PantryPilotApp.swift
+++ ios/PantryPilot/PantryPilotApp.swift
@@ -3,14 +3,30 @@ import SwiftUI
 
 @main
 struct PantryPilotApp: App {
-    var body: some Scene {
-        WindowGroup {
-            RootTabView()
-        }
-        .modelContainer(for: [
+    private let modelContainer: ModelContainer = {
+        let schema = Schema([
             StoredIngredient.self,
             Recipe.self,
             RecipeIngredient.self
         ])
+        let configuration = ModelConfiguration(
+            "PantryPilotLocalStoreV2",
+            schema: schema,
+            isStoredInMemoryOnly: false,
+            allowsSave: true
+        )
+
+        do {
+            return try ModelContainer(for: schema, configurations: [configuration])
+        } catch {
+            fatalError("Could not create Pantry Pilot model container: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            RootTabView()
+        }
+        .modelContainer(modelContainer)
     }
 }
